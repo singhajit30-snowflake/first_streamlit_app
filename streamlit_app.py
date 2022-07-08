c183494948230130a76a13b47dda86e8c7ee5664
singhajit30-snowflake <108906147+singhajit30-snowflake@users.noreply.github.com>
2022-07-08 01:33:09 +0100

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ import pandas
 my_fruit_list = pandas.read_csv("http://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 # Let's put  a pick list hero so they can pick the fruit they want to include
-streamlit.multiselect('Pick some fruits:",list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
                       
 #display the table on the page
 streamlit.dataframe(my_fruit_list)
